298f0e2643f8a829f08a771e7e62289701b83ed1
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2023-07-24 20:53:36 -0300

    Update sort.spec.ts

--- tests/ui/specs/sort.spec.ts
+++ tests/ui/specs/sort.spec.ts
@@ -1,23 +1,13 @@
 import { test, expect } from '@playwright/test';
 
-const sortOptions = {
-  az: 'Sauce Labs Backpack',
-  za: 'Test.allTheThings() T-Shirt (Red)',
-  lohi: 'Sauce Labs Onesie',
-  hilo: 'Sauce Labs Fleece Jacket',
-}
-
 test.beforeEach(async ({ page }) => {
     await page.goto('/inventory.html');
 });
 
 test.describe('Sort without Page Object Model', () => {
-  for(const option in sortOptions){
-    test(`successfully sort by ${option}`, async ({page}) => {
-      await page.getByTestId('product_sort_container')
-      .selectOption(option);
-      await expect(page.locator('#inventory_container')
-      .first()).toContainText(sortOptions[option]);
+    test(`successfully sort by hilo`, async ({page}) => {
+      await page.getByTestId('product_sort_container').selectOption('hilo');
+      await expect(page.locator('.inventory_item_price').first()).toHaveText('$49.9');
+      await expect(page.locator('.inventory_item_price').last()).toHaveText('$7.99');
     });
-  }
 });
