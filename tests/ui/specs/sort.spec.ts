ba584ed79c906921754baa1e4bf4c85697de2e89
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2023-07-24 21:00:20 -0300

    Update sort.spec.ts

--- tests/ui/specs/sort.spec.ts
+++ tests/ui/specs/sort.spec.ts
@@ -7,7 +7,7 @@ test.beforeEach(async ({ page }) => {
 test.describe('Sort without Page Object Model', () => {
     test(`successfully sort by hilo`, async ({page}) => {
       await page.getByTestId('product_sort_container').selectOption('hilo');
-      await expect(page.locator('.inventory_item_price').first()).toHaveText('$49.9');
+      await expect(page.locator('.inventory_item_price').first()).toHaveText('$49.99');
       await expect(page.locator('.inventory_item_price').last()).toHaveText('$7.99');
     });
 });
